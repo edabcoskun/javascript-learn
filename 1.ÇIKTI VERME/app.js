7f48642ccd2e90b3c50771d4ed1cc9b3ce331987
edabcoskun <coskunedab@gmail.com>
2023-07-21 18:24:52 +0300

    çıktı verme yapıldı

---
+++ 1.ÇIKTI VERME/app.js
@@ -0,0 +1,39 @@
+//*document
+
+//* document.writeln("hello world <br>");
+//* document.write("buse");
+
+
+
+//? console.log("hellöö");
+//? console.log("buse");
+//? console.log({name:"buse",surname:"çoşkun"});
+//? console.log(4.1);
+
+//? let a = 5
+//? let b= 3
+//? console.log(a+b);
+
+
+//* Alert (uyarı) poopup ile çıktı verme
+//* console.log(window);
+
+
+// alert("merhaba");
+
+// let a = 1;
+// let b = 2;
+
+// alert("iki sayının toplamı :" + (a + b));
+
+console.log(location.port);
+
+//! window un içindekilere erişirken başa window koymadan yapılabilir.Ama documnet koymak zorundasın.
+// debugger;
+console.error("hata oluştu");
+console.error("hata oluştu");
+console.error("hata oluştu");
+console.error("hata oluştu");
+console.error("hata oluştu");
+
+
